fae33bddd2160123da570751d7da3818b8a182b2
Maximo Sierra <fr3shkid16@gmail.com>
2023-01-04 10:03:51 -0500

    Move id prop to the input

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,48 +1,53 @@
-import { useEffect, useState } from "react";
-import { connect } from "react-redux";
-import logo from "../../imgs/logo.png";
-import { ITEM_FILTER_BY_TITLE } from "../../constants/actionTypes"
-import agent from "../../agent"
+import { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import logo from '../../imgs/logo.png';
+import { ITEM_FILTER_BY_TITLE } from '../../constants/actionTypes';
+import agent from '../../agent';
 
 const mapStateToProps = (state) => ({});
 
 const mapDispatchToProps = (dispatch) => ({
-  setItemsByTitle: (payload) => dispatch({ type: ITEM_FILTER_BY_TITLE, payload }),
+  setItemsByTitle: (payload) =>
+    dispatch({ type: ITEM_FILTER_BY_TITLE, payload }),
 });
 
 const Banner = ({ setItemsByTitle }) => {
-  const [canSearch, setCanSearch] = useState(false)
-  const [searchWord, setSearchWord] = useState('')
+  const [canSearch, setCanSearch] = useState(false);
+  const [searchWord, setSearchWord] = useState('');
 
   const handleChange = async (e) => {
-    if(e.target.value.length > 2){
-      setCanSearch(true)
+    if (e.target.value.length > 2) {
+      setCanSearch(true);
     }
 
-    setSearchWord(e.target.value)  
-  }
+    setSearchWord(e.target.value);
+  };
 
   const setItems = async () => {
-    if(canSearch){
-      const payload = await agent.Items.byTitle(searchWord)
-      setItemsByTitle(payload)
-    } 
-  }
+    if (canSearch) {
+      const payload = await agent.Items.byTitle(searchWord);
+      setItemsByTitle(payload);
+    }
+  };
 
-  useEffect(() => { 
-    setItems()
-  }, [canSearch, searchWord]) 
+  useEffect(() => {
+    setItems();
+  }, [canSearch, searchWord]);
 
   return (
-    <div className="banner text-white">
-      <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
+    <div className='banner text-white'>
+      <div className='container p-4 text-center'>
+        <img src={logo} alt='banner' />
         <div>
           <span>A place to </span>
-          <span id="get-part">get{" "}
-          <div id="search-box">
-            <input type="text" placeholder="What is it that you truly desire?" onChange={handleChange}/>
-          </div>
+          <span id='get-part'>
+            get{' '}
+            <input
+              id='search-box'
+              type='text'
+              placeholder='What is it that you truly desire?'
+              onChange={handleChange}
+            />
           </span>
           <span> the cool stuff.</span>
         </div>
@@ -52,4 +57,3 @@ const Banner = ({ setItemsByTitle }) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Banner);
-
